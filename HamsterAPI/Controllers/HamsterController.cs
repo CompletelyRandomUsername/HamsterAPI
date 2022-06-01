19fe8f6220c666eba18cedc617c756443ee29832
Ilya <sergeevmladshiy@gmail.com>
2022-06-01 20:12:06 +0700

    Добавьте файлы проекта.

---
+++ HamsterAPI/Controllers/HamsterController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HamsterAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HamsterController:ControllerBase
+    {
+        private static readonly string[] Types = new[]
+        {
+            "обычный хомяк", "джунгарский хомяк", "дикий лесной хомяк", "ангорский хомяк", "резиновый хомяк с пищалкой"
+        };
+
+        private static readonly string[] Rations = new[]
+        {
+            "салат", "редис", "сало"
+        };
+
+        private readonly ILogger<HamsterController> _logger;
+
+        public HamsterController(ILogger<HamsterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Hamster> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Hamster
+            {
+                Type = Types[rng.Next(Types.Length)],
+                WeightGrams = rng.Next(20, 70),
+                AgeYears = rng.Next(1, 3),
+                Ration = Rations[rng.Next(Rations.Length)],
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ HamsterAPI/Hamster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HamsterAPI
+{
+    public class Hamster
+    {
+        public string Type { get; set; }
+        public int WeightGrams { get; set; }
+        public int AgeYears { get; set; }
+        public string Ration { get; set; }
+
+    }
+}
