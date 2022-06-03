21bbea5d85b8894e8009c45a1d0fa9d57635cc53
Ilya <sergeevmladshiy@gmail.com>
2022-06-03 22:08:04 +0700

    Созданы методы AddHamster и OutHamster

--- HamsterAPI/Controllers/HamsterController.cs
+++ HamsterAPI/Controllers/HamsterController.cs
@@ -28,18 +28,42 @@ namespace HamsterAPI.Controllers
             _logger = logger;
         }
 
+        public List<Hamster> AddHamster(List<Hamster> HamsterList)
+        {
+            var rng = new Random();
+            Hamster hamster = new Hamster(Types[rng.Next(Types.Length)], rng.Next(20, 70), rng.Next(1, 3), Rations[rng.Next(Rations.Length)]);
+            HamsterList.Add(hamster);
+            return HamsterList;
+        }
+
+        public IEnumerable<Hamster> OutHamster(List<Hamster> HamsterList)
+        {
+            var rng = new Random();
+            
+                return Enumerable.Range(0, HamsterList.Count).Select(index => new Hamster
+                {
+                    Type = HamsterList[index].Type,
+                    WeightGrams = HamsterList[index].WeightGrams,
+                    AgeYears = HamsterList[index].AgeYears,
+                    Ration = HamsterList[index].Ration
+                })
+                .ToArray();
+            
+        }
+
+
         [HttpGet]
         public IEnumerable<Hamster> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new Hamster
-            {
-                Type = Types[rng.Next(Types.Length)],
-                WeightGrams = rng.Next(20, 70),
-                AgeYears = rng.Next(1, 3),
-                Ration = Rations[rng.Next(Rations.Length)],
-            })
-            .ToArray();
+            List<Hamster> HamsterList = new List<Hamster>();
+
+            foreach (var index in Enumerable.Range(5, 10))
+                AddHamster(HamsterList);
+
+
+            return OutHamster(HamsterList);
+            
         }
+        
     }
 }
--- HamsterAPI/Hamster.cs
+++ HamsterAPI/Hamster.cs
@@ -10,5 +10,19 @@ namespace HamsterAPI
         public int AgeYears { get; set; }
         public string Ration { get; set; }
 
+        public Hamster()
+        {
+            
+        }
+        public Hamster(string Type, int WeightGrams, int AgeYears, string Ration)
+        {
+            this.Type = Type;
+            this.WeightGrams = WeightGrams;
+            this.AgeYears = AgeYears;
+            this.Ration = Ration;
+        }
+
+
+
     }
 }
